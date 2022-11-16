cb3f3af29e71760493c3e2bfb6ab05f26951f3ed
sergiokano <sergiocano.design@gmail.com>
2022-11-16 13:27:09 +0100

    endpoint deletepost done

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -2,60 +2,36 @@ const Post = require("../models/Post");
 const User = require("../models/User");
 
 const PostController = {
-    async createPost(req, res) {
-        try {
-            const post = await Post.create(req.body)
-            res.status(201).send(post);
-        } catch (error) {
-            console.error(error);
-            res
-                .status(500)
-                .send({ msg: "Error: Unable to create a post", error })
-        }
-    },
-    async updatePost(req, res) {
-        try {
-            const post = await Post.findByIdAndUpdate(
-                req.params._id,
-                req.body,
-                { new: true }
-              )
-            res.status(201).send({message: "Post updated", post})
-        } catch (error) {
-            console.error(error);
-            res
-                .status(500)
-                .send({ msg: "Error: Unable to update a post", error })
-        }
-    },
-
-    // async updatePost(req, res) {
-    //     try {
-    //         const old_post = await Post.findById(req.params._id)
-    //         if(!old_post) {
-    //             return res.status(404).send({ message: `No post with id ${req.params._id}` })
-    //         }
-    //         const post = await Post.findByIdAndUpdate(
-    //             req.params._id,
-    //             { ...req.body,  userId: req.user._id},
-    //             {
-    //               new: true,
-    //             }
-    //         );
-    //         if ( old_post.post_img && post.post_img !== old_post.post_img ) {
-    //             const dir = path.resolve("./");
-    //             await unlink(path.join(dir, old_post.post_img));
-    //         }
-    //         res.status(201).send({message: "Post updated", post})
-    //     } catch (error) {
-    //         const dir = path.resolve("./");
-    //         await unlink(path.join(dir, req.body.post_img));
-    //         console.error(error)
-    //         res.status(500).send({ message: 'There was a problem updating the post', error })
-    //     }
-    // },
-
-
+  async createPost(req, res) {
+    try {
+      const post = await Post.create(req.body);
+      res.status(201).send(post);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ msg: "Error: Unable to create a post", error });
+    }
+  },
+  async updatePost(req, res) {
+    try {
+      const post = await Post.findByIdAndUpdate(req.params._id, req.body, {
+        new: true,
+      });
+      res.status(201).send({ message: "Post updated", post });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ msg: "Error: Unable to update a post", error });
+    }
+  },
+  async deletePost(req, res) {
+    try {
+      const post = await Post.findByIdAndDelete(req.params._id);
+      res.status(200).send({ message: "Post deleted", post });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ msg: "Error: Unable to delete a post", error });
+    }
+  },
 };
+
 
 module.exports = PostController;
--- routes/posts.js
+++ routes/posts.js
@@ -5,6 +5,7 @@ const { authentication } = require("../middlewares/authentication")
 
 router.post("/create",authentication, PostController.createPost);
 router.put("/update/:_id",authentication, PostController.updatePost);
+router.delete("/delete/:_id",authentication, PostController.deletePost);
 
 
 
