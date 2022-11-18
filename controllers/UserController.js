4ff5bc299e58393ff12d155cd527a8ec49dddc26
sergiokano <sergiocano.design@gmail.com>
2022-11-18 09:44:04 +0100

    fixed bug.

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,4 @@
-const User = require("../models/user");
+const User = require("../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
@@ -15,7 +15,7 @@ const UserController = {
     }
   },
 
-  
+
   async loginUser(req, res) {
     try {
       const user = await User.findOne({
